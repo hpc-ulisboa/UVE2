4e192cf6ab2ad75fb048fb3b4c0e7d1ebc33b94f
chocoflake <abeatriz.sfernandes@gmail.com>
2024-06-18 12:33:38 +0100

    started editing insns names

--- spike/riscv/insns/ss_app_ind_ofs_sg_inc.h
+++ spike/riscv/insns/ss_app_ind_ofs_sg_inc.h
@@ -1,6 +1,8 @@
+// ss.app.ind.sca.ofs.add
+
 auto &destReg = P.SU.registers[insn.uve_rd()];
 auto origin = insn.uve_mod_origin();
 
 std::visit([&](auto &reg) {
-    reg.addDynamicModifier(dynamicModifier_t(Target::Offset, dynamicBehaviour::Add, origin, &(P.SU)));
+    reg.addScatterGModifier(scatterGModifier_t(dynamicBehaviour::Inc, origin, &(P.SU)));
 }, destReg);
---
+++ spike/riscv/insns/ss_end_ind_ofs_sg_inc.h
@@ -0,0 +1,9 @@
+// ss.app.ind.sca.ofs.add
+
+auto &destReg = P.SU.registers[insn.uve_rd()];
+auto origin = insn.uve_mod_origin();
+
+std::visit([&](auto &reg) {
+    reg.addScatterGModifier(scatterGModifier_t(dynamicBehaviour::Inc, origin, &(P.SU)));
+    reg.endConfiguration();
+}, destReg);
