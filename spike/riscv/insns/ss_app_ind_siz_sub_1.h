2474b30be878dba3c4d6e335c5c6cefef8dbc947
chocoflake <abeatriz.sfernandes@gmail.com>
2024-07-23 00:58:27 +0100

    delete old sim spike

--- spike/riscv/insns/ss_app_ind_siz_sub_1.h
+++
@@ -1,6 +0,0 @@
-auto &destReg = P.SU.registers[insn.uve_rd()];
-auto origin = insn.uve_mod_origin();
-
-std::visit([&](auto &reg) {
-    reg.addDynamicModifier(dynamicModifier_t(Target::Size, dynamicBehaviour::Subtract, origin, &(P.SU), 0));
-}, destReg);
