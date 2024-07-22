7a1be53f823d384afb713a2e30865e5941c01035
chocoflake <abeatriz.sfernandes@gmail.com>
2024-07-23 00:47:00 +0100

    delete old spike

--- spike/riscv/insns/ss_app_ind_ofs_sg_dec.h
+++
@@ -1,8 +0,0 @@
-// ss.app.ind.sca.ofs.add
-
-auto &destReg = P.SU.registers[insn.uve_rd()];
-auto origin = insn.uve_mod_origin();
-
-std::visit([&](auto &reg) {
-    reg.addScatterGModifier(scatterGModifier_t(dynamicBehaviour::Decrement, origin, &(P.SU), 0));
-}, destReg);
--- spike/riscv/insns/ss_cfg_vec.h
+++
@@ -1,5 +0,0 @@
-auto &streamReg = P.SU.registers[insn.uve_rd()];
-
-std::visit([&](auto &reg) {
-    reg.configureVecDim();
-}, streamReg);
